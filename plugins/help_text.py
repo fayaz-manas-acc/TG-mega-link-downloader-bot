130cbbbaca62daaaf3a28d0b8b7e1a7c0a50a269
flmorse <69221003+flmorse@users.noreply.github.com>
2021-09-20 05:19:52 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -24,7 +24,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 REPLY_MARKUP = InlineKeyboardMarkup(
     [
-        [InlineKeyboardButton("🚀 Deploy Yours Now! 😍", url="https://github.com/XMYSTERlOUSX/mega-link-downloader-bot")],
+        [InlineKeyboardButton("༺🅹🅾️🅷🅽 🅳🅾️🅴༻", url="https://t.me/johndoelk")],
     ]
 )
 
