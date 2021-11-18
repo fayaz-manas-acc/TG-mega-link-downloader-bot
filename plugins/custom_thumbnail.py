c77dfbbc4668cb630b69361a57e4a445a2fde6a8
Will <48420466+WillTreze@users.noreply.github.com>
2021-11-18 10:57:37 -0300

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -26,18 +26,24 @@ from database.userchats import add_chat
 
 @Client.on_message(filters.photo)
 async def save_photo(bot, update):
-    if Config.UPDATE_CHANNEL:
-      try:
-          user = await bot.get_chat_member(Config.UPDATE_CHANNEL, update.from_user.id)
-          if user.status == "kicked":
-            await bot.edit_message_text(text=Translation.BANNED_USER_TEXT, message_id=fmsg.message_id)
-            return
-      except UserNotParticipant:
-          await bot.edit_message_text(chat_id=update.chat.id, text=Translation.FORCE_SUBSCRIBE_TEXT, message_id=fmsg.message_id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="😎 Join Channel 😎", url=f"https://telegram.me/{Config.UPDATE_CHANNEL}")]]))
-          return
-      except Exception:
-          await bot.edit_message_text(chat_id=update.chat.id, text=Translation.SOMETHING_WRONG, message_id=fmsg.message_id)
-          return
+    if update.from_user.id not in Config.AUTH_USERS:
+      await bot.delete_messages(chat_id=update.chat.id, message_ids=update.message_id)
+      a = await update.reply_text(text=Translation.NOT_AUTH_TXT)
+      time.sleep(5)
+      await a.delete()
+      return
+#     if Config.UPDATE_CHANNEL:
+#       try:
+#           user = await bot.get_chat_member(Config.UPDATE_CHANNEL, update.from_user.id)
+#           if user.status == "kicked":
+#             await bot.edit_message_text(text=Translation.BANNED_USER_TEXT, message_id=fmsg.message_id)
+#             return
+#       except UserNotParticipant:
+#           await bot.edit_message_text(chat_id=update.chat.id, text=Translation.FORCE_SUBSCRIBE_TEXT, message_id=fmsg.message_id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="😎 Join Channel 😎", url=f"https://telegram.me/{Config.UPDATE_CHANNEL}")]]))
+#           return
+#       except Exception:
+#           await bot.edit_message_text(chat_id=update.chat.id, text=Translation.SOMETHING_WRONG, message_id=fmsg.message_id)
+#           return
     fuser = update.from_user.id
     if check_blacklist(fuser):
         await update.reply_text("Sorry! You are Banned!")
